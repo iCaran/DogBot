fc220a34865e7b818f16c3dc863e05e19b82c29f
karan pratap shaw <pratapmorgan@gmail.com>
2023-03-15 14:24:41 +0530

    improve updater

--- day2/updater.py
+++ day2/updater.py
@@ -1,7 +1,7 @@
 import numpy as np
+from leg import leg
+from plotter import plotter
 
-
-# from leg import leg
 
 class updater:
 
@@ -9,10 +9,7 @@ class updater:
         self.angle_y_slider = angle_y_slider
         self.ax = ax
         self.fig = fig
-        self.l1 = legs[0]
-        self.l2 = legs[1]
-        self.l3 = legs[2]
-        self.l4 = legs[3]
+        self.legs = legs
 
     def update(self, val=None, event=None):
         print(val)
@@ -27,39 +24,18 @@ class updater:
         rad_y2 = np.radians(angle_y2)
         R_y2 = np.array([[np.cos(rad_y2), 0, np.sin(rad_y2)], [0, 1, 0], [-np.sin(rad_y2), 0, np.cos(rad_y2)]])
 
-        vec_new1 = np.identity(3).dot(R_y.dot(np.identity(3).dot(self.l1.foot)))
-        vec_new3 = np.identity(3).dot(R_y.dot(np.identity(3).dot(self.l3.foot)))
+        feet = []
 
-        vec_new2 = np.identity(3).dot(R_y2.dot(np.identity(3).dot(self.l2.foot)))
-        vec_new4 = np.identity(3).dot(R_y2.dot(np.identity(3).dot(self.l4.foot)))
-        # line.set_data([0, vec_new[0]], [0, vec_new[1]])
-        # line.set_3d_properties([0, vec_new[2]])
-        self.ax.clear()
-        self.ax.set_xlim([-400, 400])
-        self.ax.set_ylim([-200, 200])
-        self.ax.set_zlim([400, 0])
-        self.ax.set_xlabel('X Axis')
-        self.ax.set_ylabel('Y Axis')
-        self.ax.set_zlabel('Z Axis')
+        for l in range(len(self.legs)):
+            if l % 2 == 0:
+                feet.append(np.identity(3).dot(R_y.dot(np.identity(3).dot(self.legs[l].args[1]))))
+            else:
+                feet.append(np.identity(3).dot(R_y2.dot(np.identity(3).dot(self.legs[l].args[1]))))
 
-        self.ax.quiver(self.l1.knee_tail[0], self.l1.knee_tail[1], self.l1.knee_tail[2], [self.l1.knee[0]],
-                       [self.l1.knee[1]], [self.l1.knee[2]], arrow_length_ratio=0.1)
-        self.ax.quiver(self.l1.foot_tail[0], self.l1.foot_tail[1], self.l1.foot_tail[2], [vec_new1[0]], [vec_new1[1]],
-                       [vec_new1[2]], arrow_length_ratio=0.1)
+        plotter(self.ax).plot()
 
-        self.ax.quiver(self.l2.knee_tail[0], self.l2.knee_tail[1], self.l2.knee_tail[2], [self.l2.knee[0]],
-                       [self.l2.knee[1]], [self.l2.knee[2]], arrow_length_ratio=0.1)
-        self.ax.quiver(self.l2.foot_tail[0], self.l2.foot_tail[1], self.l2.foot_tail[2], [vec_new2[0]], [vec_new2[1]],
-                       [vec_new2[2]], arrow_length_ratio=0.1)
-
-        self.ax.quiver(self.l3.knee_tail[0], self.l3.knee_tail[1], self.l3.knee_tail[2], [self.l3.knee[0]],
-                       [self.l3.knee[1]], [self.l3.knee[2]], arrow_length_ratio=0.1)
-        self.ax.quiver(self.l3.foot_tail[0], self.l3.foot_tail[1], self.l3.foot_tail[2], [vec_new3[0]], [vec_new3[1]],
-                       [vec_new3[2]], arrow_length_ratio=0.1)
-
-        self.ax.quiver(self.l4.knee_tail[0], self.l4.knee_tail[1], self.l4.knee_tail[2], [self.l4.knee[0]],
-                       [self.l4.knee[1]], [self.l4.knee[2]], arrow_length_ratio=0.1)
-        self.ax.quiver(self.l4.foot_tail[0], self.l4.foot_tail[1], self.l4.foot_tail[2], [vec_new4[0]], [vec_new4[1]],
-                       [vec_new4[2]], arrow_length_ratio=0.1)
+        for foot in range(len(feet)):
+            new = leg(self.legs[foot].args[0], feet[foot], self.legs[foot].args[2])
+            new.plot(self.ax)
 
         self.fig.canvas.draw_idle()
